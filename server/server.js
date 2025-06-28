dca1da62dcb7d65b9cda2903faf3421003813def
SAKSHAMGUPTA123456 <sakshamgupta0155.in>
2025-06-28 11:59:37 +0530

    s

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express=require('express')
 const app=express()
+const cors=require('cors')
 const router=require('./router/router')
 const congodb=require('./database/db')
+app.use(cors())
 app.use('/m',router)
 app.get('/',(req,res)=>{
 res.send('hello server page')
